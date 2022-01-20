550164f974792b9d9801f1d29113caafdc2b8039
Ronak Hirpara <ronakh130@gmail.com>
2022-01-20 17:25:47 -0500

    commit before pull

--- src/electron.ts
+++ src/electron.ts
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron');
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1300,
+    height: 864,
     webPreferences: {
       nodeIntegration: true
     }
