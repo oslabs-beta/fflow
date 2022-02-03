28b52137c7bf3b8bf5dbaa5fff7a2ef45d6a0150
bryannadejesus <bryanna.dejesus@gmail.com>
2022-02-03 17:00:25 -0500

    Add window resizing save on app close
    Adds functionality to save application window size when app is
    closed by installing electron-store.

--- src/electron.ts
+++ src/electron.ts
@@ -1,26 +1,41 @@
 const { app, BrowserWindow } = require('electron');
-
+const Store = require('electron-store');
+const storage = new Store();
 // let dev = false;
 // if (process.env.NODE_ENV !== undefined && process.env.NODE_ENV === 'development') {
 //   dev = true
 // }
 
+function createWindow() {
+  const getWinSettings = () => {
+    const defaultBounds = [1280, 720];
+    const size = storage.get('win-size');
 
-function createWindow () {
+    if (size) return size;
+    else {
+      storage.set('win-size', defaultBounds);
+      return defaultBounds;
+    }
+  };
+
+  const bounds = getWinSettings();
+
+  const saveBounds = (bounds) => {
+    storage.set('win-size', bounds);
+  };
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    width: bounds[0],
+    height: bounds[1],
     show: false,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
     title: 'fflow',
     minWidth: 850,
     minHeight: 600,
-    backgroundColor:' #121212'
+    backgroundColor: ' #121212',
   });
-
 
   // let indexPath
   // if (dev && process.argv.indexOf('--noDevServer') === -1) {
@@ -40,11 +55,14 @@ function createWindow () {
 
   // win.loadURL(indexPath)
 
-  // uncomment out to maximise app on load 
+  // uncomment out to maximise app on load
   // win.maximize();
   win.show();
   // and load the index.html of the app.
   win.loadFile('index.html');
+  win.on('resized', () => {
+    saveBounds(win.getSize());
+  });
   // win.loadURL('http://localhost:3000/');
   // Open the DevTools.
   // win.webContents.openDevTools();
@@ -52,15 +70,15 @@ function createWindow () {
 
 // app.on('ready', createWindow);
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});
 
-// quit app when all windows are closed 
+// quit app when all windows are closed
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
 // Open a window if none are open (macOS)
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
