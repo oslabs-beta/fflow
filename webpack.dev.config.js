b2de1f86d405764cb675a78c6364939540aebe9b
crumblepie <rainlewis@icloud.com>
2022-02-04 17:37:06 -0500

    Add new webpack configs for build and dev mode

---
+++ webpack.dev.config.js
@@ -0,0 +1,83 @@
+// node: {
+//   fs: 'empty'
+// }
+
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { spawn } = require('child_process');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const APP_DIR = path.resolve(__dirname, './src');
+const MONACO_DIR = path.resolve(__dirname, './node_modules/monaco-editor');
+
+// Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
+const defaultInclude = path.resolve(__dirname, 'src');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // include: APP_DIR,
+        exclude: /node_modules/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'postcss-loader' }],
+        include: defaultInclude,
+      },
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-react'] } }],
+        include: defaultInclude,
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude,
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude,
+      },
+      {
+        test: /\.css$/,
+        include: MONACO_DIR,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.node$/,
+        use: ['node-loader'],
+      },
+    ],
+  },
+  target: 'electron-renderer',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'fflow',
+      // template: 'public/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    new MonacoWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'cheap-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    stats: {
+      colors: true,
+      chunks: false,
+      children: false,
+    },
+    before() {
+      spawn('electron', ['.'], { shell: true, env: process.env, stdio: 'inherit' })
+        .on('close', (code) => process.exit(0))
+        .on('error', (spawnError) => console.error(spawnError));
+    },
+  },
+};
