6d34b34310f86ea4a5ca19e60f739533a5c45353
bryannadejesus <bryanna.dejesus@gmail.com>
2022-02-08 12:13:56 -0500

    Add Header component testing
    Adds testing for header component rendering and button/toggle
    functionality.

--- __tests__/Header.test.js
+++ __tests__/Header.test.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { render, screen } from '../src/test-utils.js';
+import { render, screen, fireEvent } from '../src/test-utils.js';
 import Header from '../src/components/Header';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Header', () => {
-  test('renders Header component', () => {
+  test('renders Header component with Clear Project button', () => {
     render(<Header />);
-    expect(screen.getByRole('button', { name: /Clear Canvas/i }));
+    expect(screen.getByRole('button', { name: /Clear Project/i }));
+  });
+
+  test('renders Header component with light/dark mode Toggle', () => {
+    const { getByTestId } = render(<Header />);
+    expect(getByTestId('toggle-theme-btn')).toBeInTheDocument();
+  });
+
+  test('ClearProject button onClick functionality', () => {
+    const { getByTestId } = render(<Header />);
+
+    const mockFunc = jest.fn(() => true);
+
+    const button = getByTestId('clear-project-button');
+    fireEvent.click(button);
+    expect(mockFunc()).toBe(true);
+  });
+
+  test('light/dark mode Toggle onClick functionality', () => {
+    const { getByTestId } = render(<Header />);
+
+    const mockFunc = jest.fn(() => true);
+
+    const button = getByTestId('toggle-theme-btn');
+    fireEvent.click(button);
+    expect(mockFunc()).toBe(true);
   });
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import Navigation from './Navigation';
 import Body from './Body';
 import '../stylesheets/App.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Navigation/>
-      <Body/>
+    <div className='App' data-testid='app'>
+      <Navigation />
+      <Body />
     </div>
-  )
-}
+  );
+};
 
 export default App;
