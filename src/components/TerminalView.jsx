8ae18777d01e4a6800308c09020972198f0c5f6a
crumblepie <rainlewis@icloud.com>
2022-02-11 22:44:28 -0500

    Remove CSS Editor Tab

--- src/components/TerminalView.jsx
+++ src/components/TerminalView.jsx
@@ -13,7 +13,7 @@ const terminalArgs = {
   theme: {
     background: 'black',
   },
-  cursorStyle: 'bar',
+  cursorStyle: 'block',
   cursorBlink: 'block',
 };
 
@@ -43,44 +43,3 @@ const TerminalView = () => {
 };
 
 export default TerminalView;
-
-// import React, { useEffect, useState } from 'react';
-// import { FitAddon } from 'xterm-addon-fit';
-// import '../stylesheets/Terminal.css';
-
-// const Terminal = () => {
-//   // set initial terminal state to empty string
-//   let [terminalState, setTerminalState] = useState('$ ');
-
-//   const terminal = new Terminal();
-//   const fitAddon = new FitAddon();
-
-//   useEffect(() => {
-//     // render terminal onto div with id terminal
-//     terminal.loadAddon(fitAddon);
-//     terminal.open(document.getElementById("terminal"));
-
-//     console.log('this is terminalState:', terminalState);
-//   })
-
-//     terminal.onData(key => {
-//       console.log('onKey', key);
-
-//       if (key === "Backspace") {
-//           terminal.write("\b \b");
-//           setTerminalState(terminalState.slice(0,-1));
-//           console.log('backspace state', terminalState);
-//       }
-//       else {
-//         terminal.write(key)
-//         setTerminalState(terminalState += key);
-//         console.log('state', terminalState);
-//         }
-//       })
-
-//   return (
-//   <div>
-
-//   </div>
-//   )
-// };
