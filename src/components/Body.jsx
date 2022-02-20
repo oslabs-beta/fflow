5db490283571512715b9e489471c0c031c87a427
crumblepie <rainlewis@icloud.com>
2022-02-20 17:56:22 -0500

    Add comments to Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -13,34 +13,34 @@ const Body = () => {
   // to pull array of all components that were dragged on
   const components = useSelector((state) => state.canvas.components);
 
-  const dragStart = dragItem => {
-    if (dragItem.source.droppableId === 'canvas') {
-      document.getElementById(dragItem.draggableId).style.backgroundColor = '#d0f0fd';
-    }
-  }
+  const dragStart = (dragItem) => {
+    if (dragItem.source.droppableId === 'canvas') document.getElementById(dragItem.draggableId).style.backgroundColor = '#d0f0fd';
+  };
 
-  const dragEnd = dragItem => {
+  const dragEnd = (dragItem) => {
     //update state with what's dragged onto canvas
-    if (dragItem.source.droppableId === 'canvas') {
-      //if dragged from canvas
-      document.getElementById(dragItem.draggableId).style.backgroundColor = 'inherit';
-    }
+    //if dragged from canvas
+    if (dragItem.source.droppableId === 'canvas') document.getElementById(dragItem.draggableId).style.backgroundColor = 'inherit';
+
     if (!dragItem.destination) return;
     if (dragItem.source.droppableId === 'htmlTags' && dragItem.destination.droppableId === 'canvas') {
-      //if dragged from tags to canvas
+      // if dragged from tags to canvas
+      // dispatch addComponent and refreshCode reducers
       dispatch(addComponent(dragItem));
       dispatch(refreshCode());
     } else if (dragItem.source.droppableId === 'canvas' && dragItem.destination.droppableId === 'canvas') {
-      //if dragged to and from canvas
+      //if dragged to and from canvas (reordered)
+      // dispatch reorderComponent and refreshCode reducers
       dispatch(reorderComponent(dragItem));
       dispatch(refreshCode());
     }
+    // save code after any dragging
     dispatch(saveComponentCode());
-  }
+  };
 
   return (
     <div className='bodyContainer'>
-      <DragDropContext onDragEnd={dragEnd} onDragStart={dragStart}>
+      <DragDropContext onDragStart={dragStart} onDragEnd={dragEnd}>
         <DnD />
         <Canvas components={components} />
       </DragDropContext>
