7363213d467444c99e4556344aac29d89548d941
crumblepie <rainlewis@icloud.com>
2022-10-11 14:39:51 +1100

    Add test to check CompCreator component renders properly'
    :q!

--- __tests__/LeftPanel.test.js
+++ __tests__/LeftPanel.test.js
@@ -5,15 +5,26 @@ import CompCreator from '../src/components/CompCreator';
 import Tree from '../src/components/Tree';
 
 describe('Left Panel', () => {
-  test('value of comp creator input field changes', () => {
-    const { queryByPlaceholderText } = render(<CompCreator />);
-    const input = queryByPlaceholderText('Component Name');
-    fireEvent.change(input, { target: { value: 'test' } });
-    expect(input.value).toBe('test'); 
+  test('renders CompCreator component with input field and button', () => {
+    render(<CompCreator />);
+    const inputNode = screen.getByPlaceholderText('Component Name');
+    const compButton = screen.getByRole('button', {
+      name: /add/i,
+    });
+    expect(inputNode).toBeInTheDocument();
+    expect(compButton).toBeInTheDocument();
   });
 
-  test('renders the tree component which displays file tree', () => {
+  test('value of comp creator input field changes', () => {
+    const { queryByPlaceholderText } = render(<CompCreator />);
+    const userInput = queryByPlaceholderText('Component Name');
+    fireEvent.change(userInput, { target: { value: 'test' } });
+    expect(userInput.value).toBe('test');
+  });
+
+  test('renders tree component with mini file tree', () => {
     const { getByTestId } = render(<Tree />);
     expect(getByTestId('filetree')).toBeInTheDocument();
   });
 });
+
